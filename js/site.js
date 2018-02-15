998c93d5581235b7165839c2eee994a3be12bb59
bollinghball <bollinghball@gmail.com>
2018-02-15 08:35:23 -0500

    updates to text on home and shop page

---
+++ js/site.js
@@ -0,0 +1,7 @@
+// Main JS should go here!
+// Include scripts using Browserify by doing:
+// var $ = require("jquery");
+
+
+let myApiKey = config.SHOPIFY_KEY;
+let secretKey = config.MAILCHIMP_KEY;
